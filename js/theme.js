bb334fe00116855e2d83367403ecc536ec64fbad
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-07-10 16:12:30 +0200

    test change click toggler

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// смена темы сайта light or dark
 const $HTML = document.documentElement;
 let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
@@ -7,3 +9,14 @@ if(sessionStorage.getItem("theme")){
 } else{
     $HTML.dataset.theme = isDark ? "dark" : "light";
 }
+
+
+// выбираем тему сайта
+const changeTheme = function (){
+    console.log('click');
+}
+
+window.addEventListener("load", () => {
+    const $themeBtn = document.querySelector("[data-theme-toggler]");
+    $themeBtn.addEventListener("click", changeTheme);
+});

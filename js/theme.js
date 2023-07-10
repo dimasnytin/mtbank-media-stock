217081cb2a63607548b22c791049a3bd82ad992e
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-07-10 16:00:05 +0200

    add js theme

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,9 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
-
 "use strict";
+const $HTML = document.documentElement;
+let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if(sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else{
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}

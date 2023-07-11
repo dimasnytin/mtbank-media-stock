1ba06dc3ab517df63f1ef38b91b4c994f91de758
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-07-11 12:00:26 +0200

    add js global import ripple effect in global js

--- js/global.js
+++ js/global.js
@@ -1,1 +1,18 @@
+// import
+import { ripple } from "./utils/ripple.js"
+
+
+
+
 "use strict";
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+
+// добовляем эффект для ripple
+const $rippleElems = document.querySelector("[data-ripple]");
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem));
--- js/utils/ripple.js
+++ js/utils/ripple.js
@@ -1,6 +1,7 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
-
 "use strict";
+export const ripple = function ($rippleElem){
+    console.log($rippleElem);
+    $rippleElem.addEventListener("pointerdown", function(e){
+        e.stopImmediatePropagation();
+    });
+}
